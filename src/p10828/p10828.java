cca91b83b41fd7a022ef5ed57fba40a2a94fc2bd
Subeen Jeong <subin2zzxx@gmail.com>
2019-01-05 00:50:04 +0900

    stack commit

--- src/p10828/p10828.java
+++ src/p10828/p10828.java
@@ -1,9 +1,19 @@
 /**
- * push X: 정수 X를 스택에 넣는 연산이다.
- * pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * size: 스택에 들어있는 정수의 개수를 출력한다.
- * empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
- * top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+ * 14
+ * push 1
+ * push 2
+ * top
+ * size
+ * empty
+ * pop
+ * pop
+ * pop
+ * size
+ * empty
+ * pop
+ * push 3
+ * empty
+ * top
  * */
 package p10828;
 
@@ -13,5 +23,20 @@ public class p10828 {
         Stack stack = new Stack();
 
         stack.init(14);
+
+        stack.push(1);
+        stack.push(2);
+        stack.top();
+        stack.size();
+        stack.empty();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.size();
+        stack.empty();
+        stack.pop();
+        stack.push(3);
+        stack.empty();
+        stack.top();
     }
 }
